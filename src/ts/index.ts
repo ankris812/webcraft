2cef37eb105957efd87572873840f61adbe8fdcb
G'lek Tarssza <glek@glektarssza.com>
2022-10-13 15:28:24 -0600

    Use new content.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,5 @@
+import {Game} from './game';
+
 async function domReady(timeout = Infinity): Promise<void> {
     if (document.readyState === 'complete') {
         return;
@@ -31,98 +33,10 @@ async function domReady(timeout = Infinity): Promise<void> {
     });
 }
 
-function getGameCanvas(): HTMLCanvasElement {
-    let e = document.getElementById('gameCanvas') as
-        | HTMLElement
-        | null
-        | undefined;
-    if (e instanceof HTMLCanvasElement) {
-        return e;
-    }
-    e = document.getElementsByTagName('canvas')[0];
-    if (e instanceof HTMLCanvasElement) {
-        return e;
-    }
-    e = document.createElement('canvas');
-    if (e instanceof HTMLCanvasElement) {
-        document.body.appendChild(e);
-        return e;
-    }
-    throw new Error('Failed to get/create game canvas');
-}
-
 async function main(): Promise<void> {
     await domReady();
-    const canvas = getGameCanvas();
-    const gl = canvas.getContext('webgl', {
-        alpha: true,
-        antialias: true,
-        depth: true,
-        desynchronized: false,
-        failIfMajorPerformanceCaveat: false,
-        powerPreference: 'default',
-        premultipliedAlpha: true,
-        preserveDrawingBuffer: false,
-        stencil: false
-    });
-    if (gl === null) {
-        throw new Error('Failed to create rendering context');
-    }
-    const resetContext = (): void => {
-        if (
-            gl.drawingBufferWidth !== canvas.clientWidth ||
-            gl.drawingBufferHeight !== canvas.clientHeight
-        ) {
-            canvas.width = canvas.clientWidth;
-            canvas.height = canvas.clientHeight;
-            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-        }
-        gl.clearColor(0, 0, 0, 1);
-        gl.clearDepth(1);
-        gl.clear(
-            WebGLRenderingContext.COLOR_BUFFER_BIT |
-                WebGLRenderingContext.DEPTH_BUFFER_BIT
-        );
-        gl.disable(WebGLRenderingContext.DEPTH_TEST);
-    };
-    let running = false;
-    let lastFrameRequestID: number | null = null;
-    let lastFrameTimestamp: number | null = null;
-    const update = (_delta: number): void => {
-        // TODO
-    };
-    const render = (): void => {
-        // TODO
-    };
-    const nextFrame = (): void => {
-        lastFrameRequestID = null;
-        lastFrameTimestamp ??= performance.now();
-        const delta = performance.now() - lastFrameTimestamp;
-        update(delta);
-        resetContext();
-        render();
-        if (running) {
-            lastFrameRequestID = requestAnimationFrame(nextFrame);
-            lastFrameTimestamp = performance.now();
-        }
-    };
-    const start = (): void => {
-        running = true;
-        if (lastFrameRequestID === null) {
-            lastFrameRequestID = requestAnimationFrame(nextFrame);
-            lastFrameTimestamp = performance.now();
-        }
-    };
-    // const stop = (): void => {
-    //     running = false;
-    //     if (lastFrameRequestID !== null) {
-    //         cancelAnimationFrame(lastFrameRequestID);
-    //         lastFrameRequestID = null;
-    //         lastFrameTimestamp = null;
-    //     }
-    // };
-    resetContext();
-    start();
+    const game = new Game();
+    game.start();
 }
 
 main()
