0a302a43cb58ad55bedf63150d8ad47ef2d9a46a
G'lek Tarssza <glek@glektarssza.com>
2022-10-25 13:41:24 -0600

    Added a renderer class.

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,11 +1,18 @@
+import {Renderer} from './renderer';
+
 /**
  * The main game logic and state controller.
  */
 export class Game {
     /**
+     * The main game renderer.
+     */
+    public readonly renderer: Renderer;
+
+    /**
      * Create a new instance.
      */
     public constructor() {
-        // TODO
+        this.renderer = new Renderer();
     }
 }
---
+++ src/ts/renderer.ts
@@ -0,0 +1,11 @@
+/**
+ * The main game renderer.
+ */
+export class Renderer {
+    /**
+     * Create a new instance.
+     */
+    public constructor() {
+        // TODO
+    }
+}
